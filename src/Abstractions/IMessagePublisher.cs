147de53642de29886d2c94f0ab08360a8992e6e2
sa-es-ir <s.esmaeelinejad@gmail.com>
2023-11-06 23:16:38 +0700

    add method description

--- src/Abstractions/IMessagePublisher.cs
+++ src/Abstractions/IMessagePublisher.cs
@@ -8,10 +8,10 @@ public interface IMessagePublisher
     /// <summary>
     /// Publish a message to a queue based on message type
     /// </summary>
-    /// <typeparam name="T">type of message</typeparam>
-    /// <param name="message">message object</param>
-    /// <param name="priority">message priority in the queue, higher priority will process sooner</param>
-    /// <param name="keepAliveTime">message expiration in the queue (after that message will be deleted if not processed)</param>
+    /// <typeparam name="T">Type of message</typeparam>
+    /// <param name="message">Message object</param>
+    /// <param name="priority">Message priority in the queue, higher priority will process sooner</param>
+    /// <param name="keepAliveTime">Message expiration in the queue (after that message will be deleted if not processed)</param>
     /// <returns></returns>
     Task PublishAsync<T>(T message, int priority = 1, TimeSpan? keepAliveTime = null) where T : class;
 }
