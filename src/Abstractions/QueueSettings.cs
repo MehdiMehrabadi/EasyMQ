80b089b74112fef683d7016e3b0577b44422c9b7
sa-es-ir <s.esmaeelinejad@gmail.com>
2023-11-06 23:17:21 +0700

    add queue setting description

--- src/Abstractions/QueueSettings.cs
+++ src/Abstractions/QueueSettings.cs
@@ -10,10 +10,10 @@ public class QueueSettings
     /// <summary>
     /// Create a queue
     /// </summary>
-    /// <typeparam name="T">type of the message you want to enqueue</typeparam>
-    /// <param name="queueName">queue name</param>
-    /// <param name="prefetchCount">number of messages that app will fetch from rabbit</param>
-    /// <param name="retryCount">number of retry to process message in case of any errors</param>
+    /// <typeparam name="T">Type of the message you want to enqueue</typeparam>
+    /// <param name="queueName">Queue name</param>
+    /// <param name="prefetchCount">Number of messages that app will fetch from rabbit</param>
+    /// <param name="retryCount">Number of retry to process message in case of any errors</param>
     public void Add<T>(string queueName = null, int prefetchCount = 1, int retryCount = 1) where T : class
     {
         var type = typeof(T);
